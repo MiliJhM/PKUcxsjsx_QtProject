544963c79d29ade85f7e7ebc42295b4da442dfb7
Jiahao Ma <111952259+MiliJhM@users.noreply.github.com>
2023-06-01 19:18:48 +0800

    Class defined: Menu Window

--- QStartupMenuWindow.cpp
+++ QStartupMenuWindow.cpp
@@ -1,20 +1,16 @@
 #include "QStartupMenuWindow.h"
-
+#define pass  
 QStartupMenuWindow::QStartupMenuWindow(QMainWindow *parent)
     : QMainWindow(parent)
 {
     setWindowTitle("Chess Game");
 
-    startButton = new QPushButton("Start");
-    difficultyButton = new QPushButton("Difficulty");
-    difficultyDisplayer = new QLCDNumber();
-    QVBoxLayout* layout = new QVBoxLayout;
-    layout->addWidget(startButton);
-    layout->addWidget(difficultyButton);
-
-    setLayout(layout);
-    connect(difficultyButton, &QPushButton::clicked, &QStartupMenuWindow::changeDifficulty);
-    connect(this, &QStartupMenuWindow::difficultyChanged, difficultyDisplayer, qOverload<int>(&QLCDNumber::display));
+    ui.setupUi(this);
+    difficulty = 0;
+    connect(ui.DifficultyButton, &QPushButton::clicked, this, &QStartupMenuWindow::changeDifficulty);
+    connect(this, &QStartupMenuWindow::difficultyChanged, ui.lcdNumber, qOverload<int>(&QLCDNumber::display));
+    connect(ui.LoadButton, &QPushButton::clicked, this, &QStartupMenuWindow::loadGame);
+    connect(ui.StartButton, &QPushButton::clicked, this, &QStartupMenuWindow::startNew);
     
 }
 
@@ -27,6 +23,29 @@ void QStartupMenuWindow::changeDifficulty() {
         difficulty = 0;
     }
     emit difficultyChanged(difficulty);
+    qDebug("[INFO]: ChangeDifficulty:%d", difficulty);
     return;
 }
 
+void QStartupMenuWindow::startNew() {
+    qDebug("[INFO]: NewGameStarted@Difficulty %d", difficulty);
+    emit newStarted(difficulty);
+    startupCompleted();
+}
+
+void QStartupMenuWindow::loadGame() {
+    QString fileName = QFileDialog::getOpenFileName(this, QStringLiteral("´æµµ"), "F:", QStringLiteral("´æµµÎÄ¼þ(*.dat)"));
+    QFile loadFile(fileName, this);
+    if (loadFile.exists()) {
+        emit gameLoaded(loadFile);
+
+        qDebug("[INFO]: LoadGameFrom: %s", fileName.toStdString().c_str());
+    }
+    else
+        qDebug("[WARNING]: LoadFailed: loadFileNOTExist");
+}
+
+void QStartupMenuWindow::startupCompleted() {
+    qDebug("[INFO]: StartupCompleted, MenuWindowClosed");
+    close();
+}
--- QStartupMenuWindow.h
+++ QStartupMenuWindow.h
@@ -4,15 +4,15 @@
 #include "ui_QtWidgetsApplication1.h"
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QFile>
+#include <QFileDialog>
+
 
 class QStartupMenuWindow : public QMainWindow
 {
     Q_OBJECT
 private:
     Ui::QStartupMenuWindow ui;
-    QPushButton* startButton;
-    QPushButton* difficultyButton;
-    QLCDNumber* difficultyDisplayer;
     size_t difficulty;
     
 
@@ -24,10 +24,14 @@ public:
 
 
 public slots:
-
     void changeDifficulty();
-
+    void startNew();
+    void loadGame();
+    void startupCompleted();
 
 signals:
     void difficultyChanged(size_t diff);
+    void newStarted(const int& difficulty);
+    void gameLoaded(const QFile& binSL);
+
 };
